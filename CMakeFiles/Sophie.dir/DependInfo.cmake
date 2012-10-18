a40675f960e7747e99971b0e66735cb80f6b92ba
chiwawa <duong.antoine@gmail.com>
2012-10-18 15:52:09 +0200

    Cmake update

---
+++ CMakeFiles/Sophie.dir/DependInfo.cmake
@@ -0,0 +1,16 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Chiwawa/Documents/cpp/SophieGit/Sophie/main.cpp" "/Users/Chiwawa/Documents/cpp/SophieGit/Sophie/CMakeFiles/Sophie.dir/main.cpp.o"
+  "/Users/Chiwawa/Documents/cpp/SophieGit/Sophie/src/Acceptor/Acceptor.cpp" "/Users/Chiwawa/Documents/cpp/SophieGit/Sophie/CMakeFiles/Sophie.dir/src/Acceptor/Acceptor.cpp.o"
+  "/Users/Chiwawa/Documents/cpp/SophieGit/Sophie/tools/Socket/SockTraits.cpp" "/Users/Chiwawa/Documents/cpp/SophieGit/Sophie/CMakeFiles/Sophie.dir/tools/Socket/SockTraits.cpp.o"
+  "/Users/Chiwawa/Documents/cpp/SophieGit/Sophie/tools/Socket/TCPPolicy.cpp" "/Users/Chiwawa/Documents/cpp/SophieGit/Sophie/CMakeFiles/Sophie.dir/tools/Socket/TCPPolicy.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ CMakeFiles/Sophie.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Sophie.dir/tools/Socket/SockTraits.cpp.o"
+  "CMakeFiles/Sophie.dir/tools/Socket/TCPPolicy.cpp.o"
+  "CMakeFiles/Sophie.dir/src/Acceptor/Acceptor.cpp.o"
+  "CMakeFiles/Sophie.dir/main.cpp.o"
+  "Sophie.pdb"
+  "Sophie"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Sophie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
